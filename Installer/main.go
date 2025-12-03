b254cfc7faaeac4d08788e44cb9e63eb50af4045
Atx241 <atticusstewart241@gmail.com>
2025-12-02 18:19:36 -0700

    Added uninstallation capabilities

--- Installer/main.go
+++ Installer/main.go
@@ -17,30 +17,46 @@ var bepinexFile []byte
 //go:embed MultiAssembly.dll
 var multiAssemblyFile []byte
 
+var ps = string(os.PathSeparator)
+
+var ioReader = bufio.NewReader(os.Stdin)
+
+// Change for different operating systems
 var aviassemblyFolder = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Aviassembly"
 
 func main() {
 	if _, err := os.Stat(aviassemblyFolder); os.IsNotExist(err) {
 		fmt.Println("Aviassembly folder does not exist")
 		hangAndClose(1)
-		return
+	}
+	if _, err := os.Stat(aviassemblyFolder + ps + "BepInEx" + ps + "plugins" + ps + "MultiAssembly.dll"); !os.IsNotExist(err) {
+		fmt.Println("Mod already installed. Would you like to uninstall it? (y/n)")
+		if v, _ := ioReader.ReadString('\n'); v[0] == 'y' {
+			err := os.Remove(aviassemblyFolder + ps + "BepInEx" + ps + "plugins" + ps + "MultiAssembly.dll")
+			if err != nil {
+				fmt.Println("Failed to uninstall MultiAssembly. Try to run the installer in administrator mode")
+				hangAndClose(1)
+			}
+			fmt.Println("Successfully uninstalled MultiAssembly")
+			hangAndClose(0)
+		}
 	}
 	fmt.Println("Aviassembly folder found. Extracting...")
 	extBepinex()
 	extMod()
 	fmt.Println("--------")
 	fmt.Println("Successfully installed the MultiAssembly mod")
-	fmt.Println("Please start and quit Aviassembly to finish setup. The mod should be functioning afterwards.")
+	fmt.Println("Please start and quit Aviassembly once to finish setup. The mod should be functioning afterwards.")
 	hangAndClose(0)
 }
 
 func hangAndClose(code int) {
 	fmt.Println("Press any key to close...")
-	bufio.NewReader(os.Stdin).ReadRune()
+	ioReader.ReadRune()
 	os.Exit(code)
 }
 func extBepinex() {
-	if _, err := os.Stat(aviassemblyFolder + "\\BepInEx"); !os.IsNotExist(err) {
+	if _, err := os.Stat(aviassemblyFolder + ps + "BepInEx"); !os.IsNotExist(err) {
 		return
 	}
 	fmt.Println("BepInEx does not exist, creating...")
@@ -75,10 +91,10 @@ func extBepinex() {
 
 }
 func extMod() {
-	if _, err := os.Stat(aviassemblyFolder + "\\BepInEx\\plugins"); os.IsNotExist(err) {
-		os.Mkdir(aviassemblyFolder+"\\BepInEx\\plugins", 0777)
+	if _, err := os.Stat(aviassemblyFolder + ps + "BepInEx" + ps + "plugins"); os.IsNotExist(err) {
+		os.Mkdir(aviassemblyFolder+ps+"BepInEx"+ps+"plugins", 0777)
 	}
-	err := os.WriteFile(aviassemblyFolder+"\\BepInEx\\plugins\\MultiAssembly.dll", multiAssemblyFile, 0777)
+	err := os.WriteFile(aviassemblyFolder+ps+"BepInEx"+ps+"plugins"+ps+"MultiAssembly.dll", multiAssemblyFile, 0777)
 	if err != nil {
 		fmt.Println("Failed to write MultiAssembly dll to the BepInEx plugins folder")
 		hangAndClose(1)
